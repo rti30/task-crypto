b47275dc57c8acb06f9a8f7cfda67a72c62d730e
rti30 <it.development.r@gmail.com>
2022-05-08 12:43:49 +0400

    скрыл точки из графика

--- src/components/converter/ConverterChart.vue
+++ src/components/converter/ConverterChart.vue
@@ -43,6 +43,7 @@ export default {
             borderColor:
               "#" +
               (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6),
+            pointRadius: 0,
           },
         ],
       };
